1235d9536d48720c393ed26abf1bbf40d4add7fd
Daria <dariab51261@gmail.com>
2025-06-24 02:00:09 +0400

    Добавила взаимодействие с JavaFX. Создала Branch

---
+++ src/main/java/models/Branch.java
@@ -0,0 +1,17 @@
+package models;
+public class Branch {
+    private int branchId;
+    private String branchName;
+    private String address;
+    private String phone;
+
+    // Геттеры и сеттеры
+    public int getBranchId() { return branchId; }
+    public void setBranchId(int branchId) { this.branchId = branchId; }
+    public String getBranchName() { return branchName; }
+    public void setBranchName(String branchName) { this.branchName = branchName; }
+    public String getAddress() { return address; }
+    public void setAddress(String address) { this.address = address; }
+    public String getPhone() { return phone; }
+    public void setPhone(String phone) { this.phone = phone; }
+}
