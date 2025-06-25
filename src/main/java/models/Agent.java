d0ff1db1cc34a988c1a1b82bac80d627a249f263
Daria <dariab51261@gmail.com>
2025-06-26 00:10:34 +0400

    Успешно добавлен

---
+++ src/main/java/models/Agent.java
@@ -0,0 +1,30 @@
+package models;
+
+public class Agent {
+    private int agentId;
+    private int branchId;
+    private String lastName;
+    private String firstName;
+    private String middleName;
+    private String phone;
+    private double salary;
+    private double commissionRate;
+
+    // Геттеры и сеттеры
+    public int getAgentId() { return agentId; }
+    public void setAgentId(int agentId) { this.agentId = agentId; }
+    public int getBranchId() { return branchId; }
+    public void setBranchId(int branchId) { this.branchId = branchId; }
+    public String getLastName() { return lastName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
+    public String getFirstName() { return firstName; }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+    public String getMiddleName() { return middleName; }
+    public void setMiddleName(String middleName) { this.middleName = middleName; }
+    public String getPhone() { return phone; }
+    public void setPhone(String phone) { this.phone = phone; }
+    public double getSalary() { return salary; }
+    public void setSalary(double salary) { this.salary = salary; }
+    public double getCommissionRate() { return commissionRate; }
+    public void setCommissionRate(double commissionRate) { this.commissionRate = commissionRate; }
+}
